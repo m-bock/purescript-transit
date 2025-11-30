8efca2eed9075257103ede0222d69230d4d12255
Michael Bock <no-day@posteo.net>
2025-11-30 11:56:52 +0100

    Add example graph

--- src/Transit/Gen/Graphviz.purs
+++ src/Transit/Gen/Graphviz.purs
@@ -2,6 +2,13 @@ module Transit.Gen.Graphviz where
 
 import Prelude
 
+import Color (rgb)
+import Data.DotLang (node, (=*>), (==>))
+import Data.DotLang as Dot
+import Data.DotLang.Attr (FillStyle(..))
+import Data.DotLang.Attr.Edge as Edge
+import Data.DotLang.Attr.Node as Node
+import Data.DotLang.Class (toText)
 import Effect (Effect)
 import Node.Encoding (Encoding(..))
 import Node.FS.Sync as FS
@@ -10,6 +17,13 @@ import Transit.Core (StateGraph_(..))
 import Unsafe.Coerce (unsafeCoerce)
 
 type GraphizGraph = {}
+
+exampleGraph = Dot.DiGraph
+  [ node "a" [ Node.Shape Node.Diamond, Node.Style Node.Filled, Node.FillColor (rgb 255 0 0) ]
+  , node "b" []
+  , "a" ==> "b"
+  , "a" =*> "d" $ [ Edge.FillColor (rgb 255 0 0) ]
+  ]
 
 f :: StateGraph_ -> GraphizGraph
 f = unsafeCoerce "todo"
@@ -30,4 +44,4 @@ h :: forall @spec. (Options -> Options) -> FilePath -> Effect Unit
 h = unsafeCoerce "todo"
 
 writeToFile :: forall @spec. FilePath -> Effect Unit
-writeToFile path = FS.writeTextFile UTF8 path "digraph Sample { a -> b; }"
+writeToFile path = FS.writeTextFile UTF8 path (toText exampleGraph)
